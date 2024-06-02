19f2a52655a09c4e507a5926625d754cb02bc649
Rui Pereira <r_f@live.com.pt>
2024-06-03 00:38:43 +0100

    update

--- bo-jrpecas/src/pages/api/orders.js
+++ bo-jrpecas/src/pages/api/orders.js
@@ -17,7 +17,7 @@ export async function getProducts() {
     const data = await response.json()
     return data
   } catch (error) {
-    console.error('Failed to fetcshss data:', error)
+    console.error('Failed to fetcshsss data:', error)
     throw error
   }
 }
