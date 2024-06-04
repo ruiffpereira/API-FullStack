6108f6a217bdcaf2685deefff703f8111ea3ad4a
Rui Pereira <r_f@live.com.pt>
2024-06-04 23:53:03 +0100

    update bo

--- bo-jrpecas/src/pages/clients/[id].js
+++ bo-jrpecas/src/pages/clients/[id].js
@@ -1,10 +1,42 @@
-function Client(props) {
-  return <h1>{props.name.name}</h1>
+import { Fragment } from 'react'
+import { getOrders } from '../api/orders'
+
+function Client({ client, ordersfromclient }) {
+  return (
+    <Fragment>
+      <div className="max-w-sm bg-white rounded-lg shadow-md overflow-hidden mb-2">
+        <div className="flex items-center p-6">
+          <div>
+            <h2 className="text-xl font-bold text-gray-900">{client.name}</h2>
+            <div className="mt-2 flex gap-2">
+              <span className="block font-bold text-gray-600">Email</span>
+              <p className="text-gray-600">{client.email}</p>
+            </div>
+            <div className="mt-2 flex gap-2">
+              <span className="block font-bold text-gray-600">Contacto</span>
+              <p className="text-gray-600">{client.contact}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      Table com as encomendas do cliente
+    </Fragment>
+  )
 }
 
 export default Client
 
 export async function getServerSideProps(context) {
-  const name = context.query
-  return { props: { name } }
+  try {
+    const client = context.query
+    const [ordersfromclient] = await Promise.all([getOrders()])
+    return {
+      props: { ordersfromclient, client },
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error)
+    return {
+      props: { error },
+    }
+  }
 }
