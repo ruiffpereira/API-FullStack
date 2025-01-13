c855d574031cd35ed7bdb2e51728cf312d381fed
Rui Pereira <r_f@live.com.pt>
2025-01-13 21:11:23 +0000

    db update

--- migrations/20250111010002-add-newColumn-to-users.js
+++ migrations/20250111010002-add-newColumn-to-users.js
@@ -3,12 +3,12 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('Users', 'secretkeysite', {
-      type: Sequelize.STRING,
-      allowNull: true,
-    });
+    // await queryInterface.addColumn('Users', 'secretkeysite', {
+    //   type: Sequelize.STRING,
+    //   allowNull: true,
+    // });
   },
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Users', 'secretkeysite');
+    // await queryInterface.removeColumn('Users', 'secretkeysite');
   }
 };
