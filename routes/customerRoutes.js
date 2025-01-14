593d4a461942fda65152e093d388b776140d9fe0
Rui <rui@Air-de-Rita.Home>
2025-01-14 22:34:37 +0000

    update

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -3,10 +3,10 @@ const { getAllCustomers, getCustomerById, loginCustomer, updateCustomer, deleteC
 const router = express.Router();
 const { authenticateToken, authorizePermissions } = require('../src/middleware/auth');
 
-router.get('/', authenticateToken, authorizePermissions(['VIEW_CUSTOMERS']), getAllCustomers);
-router.get('/:id', authenticateToken, authorizePermissions(['VIEW_CUSTOMERS']), getCustomerById);
+// router.get('/', authenticateToken, authorizePermissions(['VIEW_CUSTOMERS']), getAllCustomers);
+// router.get('/:id', authenticateToken, authorizePermissions(['VIEW_CUSTOMERS']), getCustomerById);
 router.post('/', loginCustomer);
-router.put('/:id', authenticateToken, authorizePermissions(['VIEW_CUSTOMERS']), updateCustomer);
-router.delete('/:id', authenticateToken, authorizePermissions(['VIEW_CUSTOMERS']), deleteCustomer);
+// router.put('/:id', authenticateToken, authorizePermissions(['VIEW_CUSTOMERS']), updateCustomer);
+// router.delete('/:id', authenticateToken, authorizePermissions(['VIEW_CUSTOMERS']), deleteCustomer);
 
 module.exports = router;
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET
-const { UserPermission, Component, ComponentPermission, Permission } = require('../../models');
+const { UserPermission, Component, ComponentPermission, Permission, User } = require('../../models');
 
 const authenticateToken = (req, res, next) => {
   const token = req.headers['authorization']?.split(' ')[1];
@@ -18,6 +18,47 @@ const authenticateToken = (req, res, next) => {
 
     next();
   });
+};
+
+const authenticateTokenCustomers = async (req, res, next) => {
+  const token = req.headers['authorization']?.split(' ')[1];
+  const customerId = req.params.customerId; // Supondo que o customerId seja passado como um parâmetro de rota
+
+  if (!token) {
+    return res.sendStatus(401); // Unauthorized
+  }
+
+  try {
+    // Buscar o userId usando o customerId
+    const customer = await Customer.findOne({ where: { id: customerId } });
+    if (!customer) {
+      return res.sendStatus(404); // Not Found
+    }
+
+    const userId = customer.userId;
+
+    // Buscar a secretKey usando o userId
+    const user = await User.findOne({ where: { id: userId } });
+    if (!user) {
+      return res.sendStatus(404); // Not Found
+    }
+
+    const secretKey = user.secretKey; // Supondo que a chave secreta esteja armazenada no campo secretKey do usuário
+
+    // Validar o token usando a secretKey
+    jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        console.log("Token inválido");
+        return res.sendStatus(403); // Forbidden
+      }
+
+      req.user = user.userId;
+      next();
+    });
+  } catch (error) {
+    console.error("Erro ao autenticar o token:", error);
+    return res.sendStatus(500); // Internal Server Error
+  }
 };
 
 const authorizePermissions = (requiredPermissions) => {
@@ -56,4 +97,4 @@ const authorizePermissions = (requiredPermissions) => {
   };
 };
 
-module.exports = {authenticateToken, authorizePermissions};
+module.exports = {authenticateToken, authorizePermissions, authenticateTokenCustomers};
