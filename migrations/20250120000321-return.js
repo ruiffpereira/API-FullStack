0ff941eb5f6db71ec10b9cd709144344fe52ba87
Rui Pereira <r_f@live.com.pt>
2025-03-13 15:40:09 +0000

    update

--- migrations/20250120000321-return.js
+++ migrations/20250120000321-return.js
@@ -3,19 +3,19 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
 
-    // Truncar as tabelas Orders e OrderProducts
-    await queryInterface.bulkDelete('OrderProducts', null, { cascade: true });
-    await queryInterface.bulkDelete('Orders', null, { cascade: true });
+    // // Truncar as tabelas Orders e OrderProducts
+    // await queryInterface.bulkDelete('OrderProducts', null, { cascade: true });
+    // await queryInterface.bulkDelete('Orders', null, { cascade: true });
 
-    // Adicionar coluna userId de volta à tabela Orders
-    await queryInterface.addColumn('Orders', 'userId', {
-      type: Sequelize.UUID,
-      allowNull: false,
-      references: {
-        model: 'Users',
-        key: 'userId',
-      },
-    });
+    // // Adicionar coluna userId de volta à tabela Orders
+    // await queryInterface.addColumn('Orders', 'userId', {
+    //   type: Sequelize.UUID,
+    //   allowNull: false,
+    //   references: {
+    //     model: 'Users',
+    //     key: 'userId',
+    //   },
+    // });
   },
 
   down: (queryInterface, Sequelize) => {
