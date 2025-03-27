b211999156a3705c26010efa8f226353ae48bd54
Rui Pereira <r_f@live.com.pt>
2025-03-27 23:28:08 +0000

    update cart and ship

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,5 +1,51 @@
 const { Cart, CartProduct, Product } = require('../models');
 
+const calculateCartTotals = async (cart) => {
+  
+  const cartProducts = await Cart.findOne({
+    where: { cartId: cart.cartId }, // Filtrar pelo cartId
+    attributes: [],
+    include: [
+      {
+        model: CartProduct, // Incluir os itens do carrinho
+        as: 'cartProducts',
+        attributes: ['quantity'],
+        include: [
+          {
+            model: Product, // Incluir os detalhes dos produtos
+            as: 'product',
+            attributes: ['productId', 'name', 'price', 'photos'], // Selecionar os campos desejados
+          },
+        ],
+      },
+    ],
+    order: [
+      // Ordenar os produtos pelo nome em ordem crescente
+      [{ model: CartProduct, as: 'cartProducts' }, { model: Product, as: 'product' }, 'name', 'ASC'],
+    ],
+  });
+
+  let shipPrice = 0; // Inicializar o valor de shipPrice
+  const products = cartProducts.cartProducts.map(cartProduct => {
+
+    const productPrice = parseFloat(cartProduct.product.price); // Garantir que o preço seja um número
+    const quantity = cartProduct.quantity;
+
+    // Calcular o preço total para este produto e adicionar ao shipPrice
+    shipPrice += productPrice * quantity;
+
+    return {
+      productId: cartProduct.product.productId,
+      name: cartProduct.product.name,
+      price: cartProduct.product.price,
+      photos: cartProduct.product.photos,
+      quantity: cartProduct.quantity,
+    };
+  });
+
+  return { products, shipPrice };
+};
+
 // Adicionar produto ao carrinho
 const addProductToCart = async (req, res) => {
 
@@ -13,7 +59,7 @@ const addProductToCart = async (req, res) => {
     if (isNaN(quantityInt)) {
       return res.status(400).json({ error: 'Invalid quantity' });
     }
-    
+
     // Verificar se o cliente já tem um carrinho
     let cart = await Cart.findOne({
       where: { customerId : customerId }
@@ -23,7 +69,7 @@ const addProductToCart = async (req, res) => {
     if (!cart) {
       cart = await Cart.create({ customerId : customerId  });
     }
-  
+
     // Verificar se o produto já está no carrinho
     let cartProduct = await CartProduct.findOne({
       where: { cartId: cart.cartId, productId }
@@ -66,86 +112,13 @@ const addProductToCart = async (req, res) => {
       }
     }
 
-    const cartProducts = await Cart.findOne({
-      where: { cartId: cart.cartId }, // Filtrar pelo cartId
-      attributes: [],
-      include: [
-        {
-          model: CartProduct, // Incluir os itens do carrinho
-          as: 'cartProducts',
-          attributes: ['quantity'],
-          include: [
-            {
-              model: Product, // Incluir os detalhes dos produtos
-              as: 'product',
-              attributes: ['productId', 'name', 'price', 'photos'], // Selecionar os campos desejados
-            },
-          ],
-        },
-      ],
-      order: [
-        // Ordenar os produtos pelo nome em ordem crescente
-        [{ model: CartProduct, as: 'cartProducts' }, { model: Product, as: 'product' }, 'name', 'ASC'],
-      ],
-    });
-    
-    const formattedCartProducts =  cartProducts.cartProducts.map(cartProduct => {
-      return {
-        productId: cartProduct.product.productId,
-        name: cartProduct.product.name,
-        price: cartProduct.product.price,
-        photos: cartProduct.product.photos,
-        quantity: cartProduct.quantity,
-      };
-    });
+    const cartUpdated = await calculateCartTotals(cart);
 
-    res.status(200).json(formattedCartProducts);
+    res.status(200).json(cartUpdated);
+
   } catch (error) {
     console.error('Error adding product to cart:', error);
     res.status(500).json({ error: 'An error occurred while adding the product to the cart' });
-  }
-};
-
-// Atualizar quantidade de produto no carrinho
-const updateProductQuantityInCart = async (req, res) => {
-  const { productId, quantity } = req.body;
-  const { customerId } = req.data;
-
-  try {
-    // Converter a quantidade para um número inteiro
-    const quantityInt = parseInt(quantity, 10);
-
-    if (isNaN(quantityInt) || quantityInt < 0) {
-      return res.status(400).json({ error: 'Invalid quantity' });
-    }
-
-    // Buscar o carrinho associado ao customerId
-    const cart = await Cart.findOne({
-      where: { customerId }
-    });
-
-    const cartProduct = await CartProduct.findOne({
-      where: { cartId: cart.cartId, productId }
-    });
-
-    if (!cartProduct) {
-      return res.status(404).json({ error: 'Product not found in cart' });
-    }
-
-    if (quantityInt === 0) {
-      // Remover o produto do carrinho se a quantidade for 0
-      await cartProduct.destroy();
-      return res.status(200).json({ message: 'Product removed from cart' });
-    }
-
-    // Atualizar a quantidade do produto no carrinho
-    cartProduct.quantity = quantityInt;
-    await cartProduct.save();
-
-    res.status(200).json(cartProduct);
-  } catch (error) {
-    console.error('Error updating product quantity in cart:', error);
-    res.status(500).json({ error: 'An error occurred while updating the product quantity in the cart' });
   }
 };
 
@@ -163,40 +136,9 @@ const getCartItems = async (req, res) => {
       cart = await Cart.create({ customerId : customerId  });
     }
 
-    const cartProducts = await Cart.findOne({
-      where: { cartId: cart.cartId }, // Filtrar pelo cartId
-      attributes: [],
-      include: [
-        {
-          model: CartProduct, // Incluir os itens do carrinho
-          as: 'cartProducts',
-          attributes: ['quantity'],
-          include: [
-            {
-              model: Product, // Incluir os detalhes dos produtos
-              as: 'product',
-              attributes: ['productId', 'name', 'price', 'photos'], // Selecionar os campos desejados
-            },
-          ],
-        },
-      ],
-      order: [
-        // Ordenar os produtos pelo nome em ordem crescente
-        [{ model: CartProduct, as: 'cartProducts' }, { model: Product, as: 'product' }, 'name', 'ASC'],
-      ],
-    });
-    
-    const formattedCartProducts =  cartProducts.cartProducts.map(cartProduct => {
-      return {
-        productId: cartProduct.product.productId,
-        name: cartProduct.product.name,
-        price: cartProduct.product.price,
-        photos: cartProduct.product.photos,
-        quantity: cartProduct.quantity,
-      };
-    });
+    const cartUpdated = await calculateCartTotals(cart);
 
-    res.status(200).json(formattedCartProducts);
+    res.status(200).json(cartUpdated);
       
   } catch (error) {
     console.error('Error fetching cart items:', error);
@@ -206,6 +148,5 @@ const getCartItems = async (req, res) => {
 
 module.exports = {
   addProductToCart,
-  updateProductQuantityInCart,
   getCartItems,
 };
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const { addProductToCart, updateProductQuantityInCart,getCartItems } = require('../controllers/cartController.js');
+const { addProductToCart,getCartItems } = require('../controllers/cartController.js');
 const router = express.Router();
 
 router.post('/', addProductToCart);
-router.put('/', updateProductQuantityInCart);
 router.get('/', getCartItems);
 
 module.exports = router;
